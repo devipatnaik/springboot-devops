477f3c05ad2102885d7c664c80dc33b45283d444
devpatna <devpatna@host.docker.internal>
2020-03-23 12:10:13 +0530

    Project Setup completed

---
+++ springboot-devops/src/main/java/com/spain/csd/SpringbootDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.spain.csd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDevopsApplication.class, args);
+	}
+
+}
