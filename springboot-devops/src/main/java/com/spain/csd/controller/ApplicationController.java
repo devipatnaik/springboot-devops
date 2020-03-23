ddf6e03225b3ab334fe3ac54b6110ae13f747fd5
devpatna <devpatna@host.docker.internal>
2020-03-23 17:03:34 +0530

    JSP page added for View

--- springboot-devops/src/main/java/com/spain/csd/controller/ApplicationController.java
+++ springboot-devops/src/main/java/com/spain/csd/controller/ApplicationController.java
@@ -1,14 +1,14 @@
 package com.spain.csd.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping(value = "/api/v1")
 public class ApplicationController {
 	
 	@RequestMapping(value = "/myReg")
 	public String myRegistration() {
-		return "Do Registration, For DevOps Demonstration, Thank You.";
+		return "welcome";
 	}
 }
