4f7c890cf61c98b763e043bc50fe3c71ebdd9b6e
devpatna <devpatna@host.docker.internal>
2020-03-26 11:04:21 +0530

    pom file and main class updated

--- springboot-devops/src/main/java/com/spain/csd/SpringbootDevopsApplication.java
+++ springboot-devops/src/main/java/com/spain/csd/SpringbootDevopsApplication.java
@@ -2,14 +2,23 @@ package com.spain.csd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootDevopsApplication {
+public class SpringbootDevopsApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		
 		SpringApplication.run(SpringbootDevopsApplication.class, args);
 		System.out.println("main called ====");
+		
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		
+		return builder.sources(SpringbootDevopsApplication.class);
 		
 	}
 
